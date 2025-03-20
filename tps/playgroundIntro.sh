61945edee5298f6d651c76edd64edf9101d3bf4c
Draco Metallium(Rodrigo S. Cañibano) <draco.kun@gmail.com>
2025-03-20 17:37:44 -0300

    Aplicando recomendaciones `shellcheck`

--- tps/playgroundIntro.sh
+++ tps/playgroundIntro.sh
@@ -41,20 +41,20 @@ template="pg.XXXX"
 dir=$(mktemp -d -p "$PWD" -t "$template")
 
 {
-    cd "$dir"
+    cd "$dir" || exit 1
     mkdir comodines
-    cd comodines
+    cd comodines || exit 1
     for i in $(seq 1 25); do
         for b in ax bx cx aa bb cc a; do
             for ext in pdf jpg txt; do
-                file="${b}$(printf "%03d" "$i")"."$ext"
+                file="${b}$(printf "%03d" "$i").${ext}"
                 echo "Archivo $file" > "$file"
             done
         done
     done
-    cd "$dir"
+    cd "$dir" || exit 1
     mkdir grep
-    cd grep
+    cd grep || exit 1
     maxElefantes=50
     for i in $(seq 1 "$maxElefantes"); do
         file=$(printf "%02d-elefantes.txt" "$i")
